3b10729b91a925d3255dd5ed8ded98ed4c97224d
Rimsha Yousuf <rimshayousuf951@gmail.com>
2022-12-12 17:45:48 +0500

    created

---
+++ dbConfig.js
@@ -0,0 +1,14 @@
+
+const config = {
+    user: 'sa',
+    password: 'kfk9072p!',
+    server: 'localhost',
+    database :'Products',
+    options:{
+        trustedConnection: true,
+        enableArithAbort: true,
+        instancename: 'localhost',
+    },
+    port:1433
+}
+module.exports = config;
